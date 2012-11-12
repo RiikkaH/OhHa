2ad2e63654c5139135b6c49683fd6189ec3d74ce
RiikkaH <riikka.huusari@helsinki.fi>
2012-11-12 14:22:53 +0200

    Update Pong/src/pong/kayttoliittyma/NappaimistonKuuntelijaKaksinpeliin.java

--- Pong/src/pong/kayttoliittyma/NappaimistonKuuntelijaKaksinpeliin.java
+++ Pong/src/pong/kayttoliittyma/NappaimistonKuuntelijaKaksinpeliin.java
@@ -31,16 +31,20 @@ public class NappaimistonKuuntelijaKaksinpeliin implements KeyListener{
     @Override
     public void keyPressed(KeyEvent ke) {
          if(ke.getKeyCode() == KeyEvent.VK_W){
+        //ke.getChar tms??
             palkki1.liikuYlos();
         }else if(ke.getKeyCode() == KeyEvent.VK_S){
             palkki1.liikuAlas(); 
         }
         //Miksi palkki jumittaa w:llä ja s:llä?
+        //palkilla on nopeus, kun nolla ei liiku ja noissa ylhäällä vain muutetaan nopeus, jossain pelissä sitten 
+        //aina yritetään liikuttaa palkkia, mutta jos nopeus on nolla niin ihan vain ohitetaan
         component.repaint();
     }
 
     @Override
     public void keyReleased(KeyEvent ke) {
+        //nopeus muutetaan nollaan
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
